f94c7193d0c40629bb754786097861646129b03d
Askari130 <muqaddiso@gmail.com>
2025-10-19 19:45:54 +0100

    Initial commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+let firstName = 'Muqaddis'    
+let lastName = 'Olopade'      
+let country = 'Nigeria'       
+let city = 'Ikeja'         
+let age = 16                
+let isMarried = false
+
+const gravity = 10         
+const boilingPoint = 100     
+const PI = 3.14              
+
+let name = 'Askari',    
+  job = 'student',
+  live = 'Lagos'
